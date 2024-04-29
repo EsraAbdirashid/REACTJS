63e2b5fcd9b3b06e241b5d8e7e493867c0706130
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 17:40:48 +0300

    Done

--- MY_WEBSITE/src/Tijaabo.jsx
+++ MY_WEBSITE/src/Tijaabo.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Tijaabo = () => {
   return (
-    <div>Tijaabo</div>
+    <div>
+        <h6>shugri</h6>
+        <h6>hamda</h6>
+        <h6>fa'iza</h6>
+    </div>
   )
 }
 
