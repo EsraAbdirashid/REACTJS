54177d0f368aee3186b4f6b74b460fafc81c3960
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 17:41:29 +0300

    Done

--- MY_WEBSITE/src/App.jsx
+++ MY_WEBSITE/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Tijaabo from './Tijaabo'
 
 const App = () => {
   return (
     <div>
       <Header/>
+      <Tijaabo/>
       <Footer/>
+
     </div>
   )
 }
