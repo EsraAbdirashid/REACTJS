3544ea324a319c8e54c7645f280def301aa73856
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 18:20:26 +0300

    Done

--- MY_WEBSITE/src/Card.jsx
+++ MY_WEBSITE/src/Card.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-// import profilePic from './assets/About-img.png'
+import React from 'react';
+import profilePic from './assets/About-img.png';
 
 const Card = () => {
   return (
