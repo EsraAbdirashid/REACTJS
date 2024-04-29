21483cce73e32a792308309095b2c6f8ce2b6b6a
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 17:43:38 +0300

    Done

--- MY_WEBSITE/src/Footer.jsx
+++ MY_WEBSITE/src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div>
         <footer>
-        <h3>&copy; 2024</h3>
+        <h3>&copy; {new Date ().getFullYear ()}  2024</h3>
         </footer>
      
     </div>
