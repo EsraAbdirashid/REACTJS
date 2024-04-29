bf934ebbc4981f4d14046720e075508763e4b3b7
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 14:49:51 +0300

    Done

---
+++ MY_WEBSITE/src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <nav>
+                <ul>
+                        <li>@copy; 2024</li>
+                </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Footer
