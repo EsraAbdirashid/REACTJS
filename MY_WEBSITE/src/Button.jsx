8a4d9dd80b7af04efe260ecc77648abc9863a626
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 19:28:28 +0300

    Done

--- MY_WEBSITE/src/Button.jsx
+++ MY_WEBSITE/src/Button.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Button = () => {
 const styles ={
 
-                backgroundcolor: "brown";
-                color: "white";
-                padding: "20px 40px";
-                borderradius: "5px";
-                border: "none";
-                cursor: pointer;
-                font-size: large;
+                backgroundcolor: "brown",
+                color: "white",
+                padding: "20px 40px",
+                borderradius: "5px",
+                border: "none",
+                cursor: "pointer",
+                fontsize: "large",
         
 }
 
