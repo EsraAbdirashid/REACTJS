6df6dda395a57676775b0574597c1084d34694e0
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 19:27:10 +0300

    Done

--- MY_WEBSITE/src/Button.jsx
+++ MY_WEBSITE/src/Button.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const Button = () => {
+const styles ={
+
+                backgroundcolor: "brown";
+                color: white;
+                padding: 20px 40px;
+                borderradius: 5px;
+                border: none;
+                cursor: pointer;
+                font-size: large;
+        
+}
+
   return ( <button className='Button'>Click Me</button> );
 }
 
