7fa39cd5112f98b2dcec5672210301012d4fa8d5
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 18:14:54 +0300

    Done

--- MY_WEBSITE/src/Card.jsx
+++ MY_WEBSITE/src/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import
+import profilePic from './assets/About-img.png'
 
 const Card = () => {
   return (
   
      <div className='Card'>
-       <img src={} alt='profile picture'></img>
+       <img src={profilePic} alt='profile picture'></img>
         <h1>Esra Abdirashid</h1>
         <p>i am adeverloper and designer...😋</p>
  
