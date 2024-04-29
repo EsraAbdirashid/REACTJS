26b25cfe17f954b94baf4947b4cc8b98eb8bda48
EsraAbdirashid <israApdirashid@gmail.com>
2024-04-29 18:45:13 +0300

    Done

--- MY_WEBSITE/src/Card.jsx
+++ MY_WEBSITE/src/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-// import profilePic from './assets/soft.jpg';
+import profilePic from '../public/soft.JPG';
 
 const Card = () => {
   return (
   
      <div className='Card'>
-       {/* <img src={profilePic} alt="profile picture"></img> */}
-       <img src="./public/soft.JPG" alt="" />
+       <img src={profilePic} alt="profile picture"></img>
+       {/* <img src="./public/soft.JPG" alt="" /> */}
         <h1>Esra Abdirashid</h1>
         <p>i am adeverloper and designer...😋</p>
  
